240ea6809bedd93c46ece46b769e422b0b8a0505
Aibek <ai.bek@mail.ru>
2023-01-29 21:57:13 +0600

    refactor: changes root component to standalone API

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './app/app.routs';
+import { AppComponent } from './app/app.component';
 
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(RouterModule.forRoot(appRoutes)),
+  ],
+})
   .catch((err) => console.error(err));
