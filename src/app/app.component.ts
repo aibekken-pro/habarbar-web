6614af8664f84e055545e2956997c0331bff70a2
Aibek <ai.bek@mail.ru>
2023-02-10 20:26:39 +0600

    refactor: app root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
 
 @Component({
-  selector: 'app-app',
+  selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule, HttpClientModule],
+  imports: [RouterModule],
   template: '<router-outlet></router-outlet>',
 })
 export class AppComponent {
