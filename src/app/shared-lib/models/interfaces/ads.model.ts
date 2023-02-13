bb98e8345f0fee9d6c92630142c78c94913ee430
Aibek <ai.bek@mail.ru>
2023-02-13 23:32:34 +0600

    feat: add directory requests

--- src/app/shared-lib/models/interfaces/ads.model.ts
+++ src/app/shared-lib/models/interfaces/ads.model.ts
@@ -41,32 +41,3 @@ export interface IGetAd {
   lastEditDate: Date,
   moderated: boolean
 }
-
-export interface ILocation {
-  id: string;
-  location: string;
-  locationCode: string;
-  region: string;
-  regionCode: string;
-  country: string;
-  countryCode: string;
-}
-
-export interface IAddLocation {
-  location: string;
-  locationCode: string;
-  region: string;
-  regionCode: string;
-  country: string;
-  countryCode: string;
-}
-
-export interface ICategory {
-  id: string;
-  name: string;
-  subcategories?: ICategory[];
-}
-
-export interface ICreateICategory {
-  name: string;
-}
