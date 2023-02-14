c33cb1cc6b7c03b4ba40fb4499b8eec4b58361cc
Aibek <ai.bek@mail.ru>
2023-02-15 00:43:36 +0600

    fix: remove import

--- src/app/core/services/requests/ads-requests/ads-requests.service.ts
+++ src/app/core/services/requests/ads-requests/ads-requests.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { IAd, IAdCreate, IAddLocation } from '@shared-lib';
+import { IAd, IAdCreate } from '@shared-lib';
 import { Observable } from 'rxjs';
 
 @Injectable({
